fab42599043494c3aa0d6bc9fcede044f3553c8e
Alexander Nguyen <alexnguyen730@gmail.com>
2026-05-14 01:21:00 -0400

    Download packages like the orm and sqlite and tools

--- obj/Debug/net10.0/ProductsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481792b1af5355b157411003b44b5184b0845b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
